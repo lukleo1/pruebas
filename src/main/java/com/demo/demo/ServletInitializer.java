95a153903bcd5e498ed6f381932493ff211d9a14
wilmer <w.a.cotrina@gmail.com>
2018-12-10 23:55:00 -0500

    x

--- src/main/java/com/demo/demo/ServletInitializer.java
+++ src/main/java/com/demo/demo/ServletInitializer.java
@@ -17,8 +17,7 @@
 package com.demo.demo;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 public class ServletInitializer extends SpringBootServletInitializer {
 
   @Override
